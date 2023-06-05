4d632244a81cba73aea6913a71a4455dcdc4b547
Lalit Bhatia <lalit.bhatia@weightwatchers.com>
2023-06-05 09:44:27 -0400

    initial commit

---
+++ src/data/sampleData.js
@@ -0,0 +1,36 @@
+const foods = [
+    
+    {"name":"Florentine","count":972},
+    {"name":"Plain fat free yogurt","count":112},
+    {"name":"Pudding, rice","count":715},
+    {"name":"Fresh peach","count":60},
+    {"name":"Flour, whole wheat","count":481},
+    {"name":"Fresh cherries","count":238},
+    {"name":"Cheese, goat, soft type","count":404},
+    {"name":"Fish, herring","count":469},
+    {"name":"Pork loin center loin (chops) boneless lean pan-broiled","count":117},
+    {"name":"Cheesecake","count":57},
+    {"name":"Store-bought cheesecake","count":601},
+    {"name":"Lean ground lamb, raw","count":981},
+    {"name":"Apricot jam","count":844},
+    {"name":"Flan","count":292},
+    {"name":"Lemonade","count":400},
+    {"name":"Fresh lime","count":720}
+]
+
+const books = [
+    {title: "Black Dogs", author:"Ian Mcewan",year:1993},
+    {title: "The great Railway bazaar", author:"Paul Theroux",year:1979},
+    {title: "Riding the iron rooster", author:"Paul Theroux",year:1989},
+    {title: "The count of Monte  Cristo", author:"Thomas Hardy",year:1883},
+    {title: "Freedom at Midnight", author:"Larry Collins",year:1972},
+    {title: "Traveles with a Tangerine", author:"Tim Mackintosh-Smith",year:1972},
+    {title: "A house for mr Biswas", author:"V.S Naipaul",year:1962},
+    {title: "Shadows of the Silk-Road", author:"Colin Thubron",year:2001},
+    {title: "Dark-Star safari", author:"Paul Theroux",year:2001},
+    {title: "Travles of Ibn-Batutta", author:"Ross Dunn",year:1991}
+
+    
+]
+exports.foods = foods;
+exports.books = books;
---
+++ tests/trie.test.js
@@ -0,0 +1,121 @@
+
+const {search,generateTrie} = require('../src/index');
+const {foods,books} = require("../src/data/sampleData");
+
+
+describe('it should generate  trie object',() => {
+    let opts = {
+        outputProps : ["title","author"],
+        addKey:false,
+        splitRegex:"/[ -]/"
+    }
+    let trie = generateTrie(books,'title',opts)
+    it('should test that the map property exitst',() =>{
+        expect(trie.map).toBeDefined()
+    })
+    it('should test that searching \'dogs\' should return 1 item',()=>{
+        let results = search("dogs",0,trie);
+        expect(results.length).toEqual(1)
+    })
+    it('should test that searching \'the\' should return 4 items',()=>{
+        let results = search("the",0,trie);
+        // console.log(results)
+        expect(results.length).toEqual(4)
+    })
+    it('should test exclude nodes and that searching \'for\' should return 1 items',()=>{
+        let results = search("for",0,trie);
+        // console.log(results)
+        expect(results.length).toEqual(1)
+    })
+    it('should test that search results to be an array  of objects with properties \'node\' and \'nodeObj\'',()=>{
+        let results = search("the",0,trie);
+        // console.log(results)
+        results.forEach(res =>{
+            expect(res["node"]).toBeDefined();
+            expect(res["nodeObj"]).toBeDefined();
+        })                
+    })
+
+})
+
+describe('it should test input options',() => {
+    let opts = {
+        outputProps : ["title","author"],
+        addKey:false,
+        splitRegex:"/[ ]/",
+        excludeNodes : ['the','a','for']
+    }
+    let trie = generateTrie(books,'title',opts)
+    it('should test exclude nodes and that searching \'the\' should return 0 items',()=>{
+        let results = search("the",0,trie);
+        // console.log(results)
+        expect(results.length).toEqual(0)
+    })
+    it('should test exclude nodes and that searching \'for\' should return 0 items',()=>{
+        let results = search("for",0,trie);
+        // console.log(results)
+        expect(results.length).toEqual(0)
+    })
+    it('should test splitRegex option and that searching \'road\' should return 0 items',()=>{
+        let results = search("road",0,trie);
+        // console.log(results)
+        expect(results.length).toEqual(0)
+    })
+    
+    it('should test splitRegex option and that searching \'road\' should return 1 item',()=>{
+        opts.splitRegex="/[ -]/"
+        let trie = generateTrie(books,'title',opts)        
+        // console.log(results)
+        expect(search("road",0,trie).length).toEqual(1);
+        expect(search("Star",0,trie).length).toEqual(1);
+    })
+    it('should test that search is case insensetive',()=>{
+        expect(search("black d",0,trie).length).toEqual(1);
+        expect(search("Black Dog",0,trie).length).toEqual(1);
+        expect(search("Black Dog",0,trie).length).toEqual(1);
+        expect(search("BLACk dOG",0,trie).length).toEqual(1);
+    })
+    it('should test output proerties',()=>{
+        let results = search("rail",0,trie);
+        // console.log(results)
+        results.forEach(res =>{
+            expect(res["node"]).toBeDefined();
+            expect(res["nodeObj"]).toBeDefined();
+            expect(res["nodeObj"]["title"]).toBeDefined();
+            expect(res["nodeObj"]["author"]).toBeDefined();            
+            expect(res["nodeObj"]["year"]).toBeUndefined();
+        })  
+    })
+    it('should test default output proerties',()=>{
+        delete opts.outputProps;
+        let newTrie = generateTrie(books,'title',opts)
+        let results = search("rail",0,newTrie);
+        // console.log(results)
+        results.forEach(res =>{
+            expect(res["node"]).toBeDefined();
+            expect(res["nodeObj"]).toBeDefined();
+            expect(res["nodeObj"]["title"]).toBeDefined();
+            expect(res["nodeObj"]["author"]).toBeDefined();            
+            expect(res["nodeObj"]["year"]).toBeDefined();
+        })  
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+// console.log(search("herring",0,root))
+// console.log(search("fish",0,root))
+// console.log(search("cherries",0,root))
+// console.log(search("ground",0,root))
+// console.log(search("fresh",0,root))
+// console.log(search("flour",0,root))
+// console.log(search("goat",0,root))
+// console.log(search("soft",0,root))
